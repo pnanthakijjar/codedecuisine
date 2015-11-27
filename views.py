4913b295ccc6091cbd5cd1075d41853bff80206a
pnanthakijjar <poonyapornnanthakijjar@Poonyaporns-MacBook-Pro.local>
2015-11-27 00:05:18 -0500

    Updated html for most files

--- views.py
+++ views.py
@@ -1,7 +1,28 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import Flask, render_template, request, redirect
+import string
+app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index')
 def index():
-	return render_template('index.html')
+	return render_template('index.html', author=author, name=name)
+
+@app.route('/search')
+def search():
+	return render_template('search.html', author=author, name=name)
+
+@app.route('/view_recipes')
+def view_recipes():
+	return render_template('view_recipes.html', author=author, name=name)
+
+@app.route('/edit_recipes')
+def edit():
+	return render_template('edit_recipes.html', author=author, name=name)
+
+@app.route('/add_recipes')
+def add_recpies():
+	return render_template('add_recipes.html', author=author, name=name)
+
+
+
+if __name__=='__main__':
+	app.run(debug=True)
