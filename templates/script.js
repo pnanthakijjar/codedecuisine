dab40787680a9d3d5adc4c5e1130e4f712ee35e8
pnanthakijjar <poonyapornnanthakijjar@Poonyaporns-MacBook-Pro.local>
2015-11-27 22:22:37 -0500

    A lot of updates
    New CSS & JS. Trying to make the website look like recipe cards

---
+++ templates/script.js
@@ -0,0 +1,11 @@
+document.getElementById("footer").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " CodeDeCuisine. All rights reserved.</p>";
+
+document.getElementById("navbar").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='search.html'>Search Recipes</a></li>" +
+"<li><a href='view_recipes.html'>View Recipes</a></li>" +
+"<li><a href='add_recipes.html'>Add Recipes</a></li>" +
+"<li><a href='edit_recipes.html'>Edit Recipes</a></li>" +
+"</ul>";
