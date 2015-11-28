45ca4842a8cc6601a6fd4ca2eebecbfd8a314074
pnanthakijjar <poonyapornnanthakijjar@Poonyaporns-MacBook-Pro.local>
2015-11-28 01:21:17 -0500

    Some changes

---
+++ templates/form.php
@@ -0,0 +1,13 @@
+<?php
+   if(isset($_POST['submit'])) 
+   {
+      echo("Recipe Name: " . $_POST['recipe_name'] . "<br />\n");
+      echo("Ingredient: " . $_POST['ingredient'] . "<br />\n");
+      echo("Ingredient: " . $_POST['ingredient'] . "<br />\n");
+      echo("Ingredient: " . $_POST['ingredient'] . "<br />\n");
+      echo("Ingredient: " . $_POST['ingredient'] . "<br />\n");      echo("Ingredient: " . $_POST['ingredient'] . "<br />\n");
+      echo("Ingredient: " . $_POST['ingredient'] . "<br />\n");      echo("Ingredient: " . $_POST['ingredient'] . "<br />\n");
+      echo("Ingredient: " . $_POST['ingredient'] . "<br />\n");      echo("Ingredient: " . $_POST['ingredient'] . "<br />\n");
+      echo("Ingredient: " . $_POST['ingredient'] . "<br />\n");
+   }
+?>
